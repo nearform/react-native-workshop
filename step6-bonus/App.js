780319adf5c12246bf7ee6365646c2e4525dee57
Alan Slater <alan.slater@nearform.com>
2023-11-06 01:45:06 +0000

    4. Add welcome screen

--- step6-bonus/App.js
+++ step6-bonus/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Game } from "./screens/game";
+import { Welcome } from "./screens/welcome";
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -12,6 +13,7 @@ export default function App() {
     <SafeAreaProvider>
       <NavigationContainer>
         <Stack.Navigator>
+          <Stack.Screen name="Welcome" component={Welcome} />
           <Stack.Screen name="Game" component={Game} />
         </Stack.Navigator>
       </NavigationContainer>
