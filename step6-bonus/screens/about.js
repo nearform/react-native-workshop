4159101a965d7f296a45282f52a5eb135ac8dcdc
Alan Slater <alan.slater@nearform.com>
2023-11-06 01:57:25 +0000

    7. Add some web links

--- step6-bonus/screens/about.js
+++ step6-bonus/screens/about.js
@@ -1,9 +1,17 @@
-import { ScrollView, Text, StyleSheet } from 'react-native'
+import { ScrollView, Text, StyleSheet, Linking } from 'react-native'
+import { Button } from '../components/button'
 
 export const About = () => (
   <ScrollView contentContainerStyle={styles.container} style={styles.outer}>
     <Text style={styles.text}>This game was created in the NodeConf EU React Native workshop.</Text>
     <Text style={styles.text}>Tilt your device to roll the ball into the randomly-placed goals.</Text>
+    <Text style={styles.text}>Find out more:</Text>
+    <Button accessibilityRole="link" onPress={() => Linking.openURL('https://github.com/nearform/react-native-workshop')}>
+      Workshop on GitHub
+    </Button>
+    <Button accessibilityRole="link" onPress={() => Linking.openURL('https://www.nodeconf.eu/')}>
+      nodeconf.eu
+    </Button>
   </ScrollView>
 )
 
