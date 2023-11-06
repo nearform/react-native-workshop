4f2c460b8b53d21d7e56cb804b739e320e18844b
Alan Slater <alan.slater@nearform.com>
2023-11-06 01:53:58 +0000

    5. Add our own Button component

---
+++ step6-bonus/components/button.js
@@ -0,0 +1,30 @@
+import { Pressable, StyleSheet, Text } from 'react-native'
+
+export const Button = ({ children, onPress }) => (
+  <Pressable
+    style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+    accessibilityRole="button"
+    onPress={onPress}
+  >
+    <Text style={styles.text}>{children}</Text>
+  </Pressable>
+)
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#ffffff',
+    borderColor: '#000000',
+    padding: 16,
+    borderRadius: 16,
+    borderWidth: 2,
+    alignSelf: 'stretch'
+  },
+  pressed: {
+    backgroundColor: '#dddddd'
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#000000'
+  },
+})
